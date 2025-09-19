6526c12e0047c39038d759e804c182f6533bef22
Zenable Automation <automation@zenable.io>
2025-09-19 13:52:12 -0400

    feat(project): initial project generation

---
+++ src/bsidescol25/constants.py
@@ -0,0 +1,14 @@
+"""
+bsidescol25 constants
+"""
+
+import json
+
+LOG_FORMAT = json.dumps(
+    {
+        "timestamp": "%(asctime)s",
+        "namespace": "%(name)s",
+        "loglevel": "%(levelname)s",
+        "message": "%(message)s",
+    }
+)
